b12be1447d8ab2fe861f1423d49b3f8771657cbe
WC Marketplace <contact@wc-marketplace.com>
2018-06-19 18:33:46 +0530

    3rd change

--- config.rb
+++ config.rb
@@ -1,6 +1,3 @@
-# Unique header generation
-require './lib/unique_head.rb'
-
 # Markdown
 set :markdown_engine, :redcarpet
 set :markdown,
@@ -10,8 +7,7 @@ set :markdown,
     prettify: true,
     tables: true,
     with_toc_data: true,
-    no_intra_emphasis: true,
-    renderer: UniqueHeadCounter
+    no_intra_emphasis: true
 
 # Assets
 set :css_dir, 'stylesheets'
@@ -21,11 +17,6 @@ set :fonts_dir, 'fonts'
 
 # Activate the syntax highlighter
 activate :syntax
-ready do
-  require './lib/multilang.rb'
-end
-
-activate :sprockets
 
 activate :autoprefixer do |config|
   config.browsers = ['last 2 version', 'Firefox ESR']
@@ -39,19 +30,9 @@ set :relative_links, true
 
 # Build Configuration
 configure :build do
-  # If you're having trouble with Middleman hanging, commenting
-  # out the following two lines has been known to help
   activate :minify_css
   activate :minify_javascript
   # activate :relative_assets
   # activate :asset_hash
   # activate :gzip
-end
-
-# Deploy Configuration
-# If you want Middleman to listen on a different port, you can set that below
-set :port, 4567
-
-helpers do
-  require './lib/toc_data.rb'
 end
